22ea99ccffd0ce314dc69acbd8f05815927102a0
Kate <katia.yurevna@gmail.com>
2021-12-14 10:20:50 -0800

    Implementation of TextIngestor

--- src/QuoteEngine/TextIngestor.py
+++ src/QuoteEngine/TextIngestor.py
@@ -1,9 +1,19 @@
-from IngestorInterface import IngestorInterface
+from QuoteEngine.IngestorInterface import IngestorInterface
+from QuoteEngine.QuoteModel import QuoteModel
 
 
 class TextIngestor(IngestorInterface):
     @classmethod
     def parse(cls, path):
-        # TODO: add a docstring
-        print('hello there.It"s txt parser!')
-        return []
+        """TextIngestor class parses txt file and returns a list of quotes"""
+        with open(path, 'r', encoding='utf-8-sig') as text_file:
+            content = text_file.readlines()
+            for line in content:
+                quote = line.strip().split(' - ')
+                body, author = quote
+                cls.quotes.append(QuoteModel(body, author))
+
+        return cls.quotes
+
+
+# TextIngestor.parse(path='DogQuotesTXT.txt')
