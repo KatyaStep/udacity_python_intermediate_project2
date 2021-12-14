3cedc7a038c1ee9970c285012434a9bd89b03110
Kate <katia.yurevna@gmail.com>
2021-12-14 10:30:32 -0800

    Implementation of DocxIngestor

--- src/QuoteEngine/DocxIngestor.py
+++ src/QuoteEngine/DocxIngestor.py
@@ -1,9 +1,21 @@
-from IngestorInterface import IngestorInterface
+import docx
+from QuoteEngine.IngestorInterface import IngestorInterface
+from QuoteEngine.QuoteModel import QuoteModel
 
 
 class DocxIngestor(IngestorInterface):
     @classmethod
     def parse(cls, path):
-        # TODO: add a docstring
-        print('hello there.It"s docx parser!')
-        return []
+        """DocxIngestor class parses csv file and returns a list of quotes"""
+        doc = docx.Document(path)
+
+        for para in doc.paragraphs:
+            if para.text != "":
+                parse = para.text.replace('"', "").split(' - ')
+                body, author = parse
+                cls.quotes.append(QuoteModel(body, author))
+
+        return cls.quotes
+
+
+# DocxIngestor.parse(path='DogQuotesDOCX.docx')
